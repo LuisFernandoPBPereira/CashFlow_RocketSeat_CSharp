f550792f5c8b33f2acab1df5d5e974516897e21b
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2024-05-24 17:16:16 -0300

    Criando use case de despesa

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
 
@@ -6,9 +7,40 @@ public class RegisterExpenseUseCase
 {
     public RequestRegisterExpenseJson Execute(RequestRegisterExpenseJson request)
     {
+        Validate(request);
+
         return new RequestRegisterExpenseJson 
         {
 
         };
+    }
+
+    private void Validate(RequestRegisterExpenseJson request)
+    {
+        var tituloIsEmpty = string.IsNullOrWhiteSpace(request.Title);
+        var dateCompare = DateTime.Compare(request.Data, DateTime.UtcNow);
+        var isValidPaymentType = Enum.IsDefined(typeof(PaymentsType), request.PaymentsType);
+
+
+        if (tituloIsEmpty)
+        {
+            throw new ArgumentException("The title is required");
+        }
+
+        if(request.Amount <= 0)
+        {
+            throw new ArgumentException("The amount must be greater than zero");
+        }
+
+        if(dateCompare > 0)
+        {
+            throw new ArgumentException("The date can't be in the future");
+        }
+
+        if(isValidPaymentType == false)
+        {
+            throw new ArgumentException("The payment type is not valid");
+        }
+
     }
 }
